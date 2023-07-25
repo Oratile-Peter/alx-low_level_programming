cf6d941a837a4581e492b9a4764fd753a56aad0a
Oratile <Oratilesiya@gmail.com>
2023-07-25 08:45:02 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
 #include "main.h"
-
 /**
- * reset_to_98 - function that takes a pointer to an int zs parameter and updates the value it points to to 98.
- * @n:- the value to be updated to 98
- * Return: Always 0
+ * reset_to_98 - function that takes a pointer to an into
+ * as parameter and updates the value it points to to 98.
+ * @n: input
+ * Return: n
  */
-void reset_to_98(int *)
+void reset_to_98(int *n)
 {
-
-
-*n = 98:
+	*n = 98;
 }
